00845b9b13564ad6031bc2b734f0f4791813fc3c
Brian Mulloy <brian@landlessness.net>
2025-02-06 14:58:56 -0500

    R check passes. removed plots that I was playing with

--- tests/testthat/test_plot_coprime_quantum_blob.R
+++
@@ -1,8 +0,0 @@
-test_that("plot_stern_brocot_tree(10) matches reference", {
-  vdiffr::expect_doppelganger("coprime_quantum_blob",
-                              plot_coprime_quantum_blob(x = 0, p = 0,
-                                                        lower_x_uncertainty = 0.167, upper_x_uncertainty = 0.167,
-                                                        lower_p_uncertainty = 0.152, upper_p_uncertainty = 0.152,
-                                                        depth = 10)
-  )
-})
